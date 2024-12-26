ba03c5057f117929aa02c201be1deeed1d421cff
ganeshmedichelimala <ganeshmedichelimala@gmail.com>
2024-12-26 20:52:00 +0530

    added cv button

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,62 +1,112 @@
-import React from 'react';
-import "./contact.css"
+import React, { useRef } from "react";
+import "./contact.css";
 import facebook from "../../assets/facebook-icon.png";
 import twitter from "../../assets/twitter.png";
-import insta from "../../assets/instagram.png"
-import youtube from "../../assets/youtube.png"
-import github from "../../assets/github.png"
-import emailjs from '@emailjs/browser';
-import { useRef } from "react"
+import insta from "../../assets/instagram.png";
+import youtube from "../../assets/youtube.png";
+import github from "../../assets/github.png";
+import emailjs from "@emailjs/browser";
+
 const Contact = () => {
-  const form =useRef();
+  const form = useRef();
+
   const sendEmail = (e) => {
     e.preventDefault();
 
+    // Ensure these are the correct IDs from your EmailJS account
     emailjs
-      .sendForm('service_agcnztc', 'template_6eisg0e', form.current, {
-        publicKey: 'gw1b5OM0SoyWxf-vg',
-      })
+      .sendForm(
+        "service_hm76kxw",  // Replace with your correct Service ID
+        "template_tc9pdra",  // Replace with your correct Template ID
+        form.current,
+        "C4GcLWGxias0Ya_r_"  // Replace with your public API key
+      )
       .then(
-        () => {
-          console.log('SUCCESS!');
+        (result) => {
+          console.log("SUCCESS!", result.text);
         },
         (error) => {
-          console.log('FAILED...', error.text);
-        },
+          console.error("FAILED...", error.text);
+        }
       );
   };
+
   return (
-    
     <section id="contact">
-        <h2 className='contactPageTitle'>Contact <span className='m'>M</span>e</h2>
-        <span className='contactDesc'>Please fill out the form below to discuss any work opportunities</span>
-        <form className='contactform' ref={form} onSubmit={sendEmail}>
-            <input type="text" className='name' placeholder='Your GOOD Name' name='your_name'/>
-            <input type='email' className='email' placeholder='Your Email' name='your_email'/>
-            <textarea className='msg' name="message" rows="5" placeholder='Your Message'></textarea>
-            <button type="submit" value="send" className='submitBtn' >Submit</button>
-           
-            <div className='links'>
-                  <a href="https://www.facebook.com/profile.php?id=100065243855844" target="_blank" rel="noopener noreferrer">
-                      <img src={facebook} alt="FACEBOOOK" className='link'/>
-                  </a>
-                  <a href="https://x.com/m_ganesh_2946" target="_blank" rel="noopener noreferrer">
-                      <img src={twitter} alt="TWITTER" className='link'/>
-                  </a>
-                  <a href="https://www.instagram.com/ganesh_medichelimala/?hl=en" target="_blank" rel="noopener noreferrer">
-                      <img src={insta} alt="INSTAA" className='link'/>
-                  </a>
-                  <a href="https://www.youtube.com/@ganesh_2946" target="_blank" rel="noopener noreferrer">
-                      <img src={youtube} alt="YOUTUBE" className='link'/>
-                  </a>
-                  <a href="https://github.com/ganeshmedichelimala" target="_blank" rel="noopener noreferrer"> 
-                      <img src={github} alt="GITHUB" className='link'/>
-                  </a>
-             </div>
+      <h2 className="contactPageTitle">
+        Contact <span className="m">M</span>e
+      </h2>
+      <span className="contactDesc">
+        Please fill out the form below to discuss any work opportunities
+      </span>
+      <form className="contactform" ref={form} onSubmit={sendEmail}>
+        <input
+          type="text"
+          className="name"
+          placeholder="Your GOOD Name"
+          name="your_name"
+        />
+        <input
+          type="email"
+          className="email"
+          placeholder="Your Email"
+          name="your_email"
+        />
+        <input
+          type="hidden"
+          name="to_email"
+          value="ganeshmedichelimala@gmail.com"
+        />
+        <textarea
+          className="msg"
+          name="message"
+          rows="5"
+          placeholder="Your Message"
+        ></textarea>
+        <button type="submit" value="send" className="submitBtn">
+          Submit
+        </button>
 
-        </form>
+        <div className="links">
+          <a
+            href="https://www.facebook.com/profile.php?id=100065243855844"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={facebook} alt="FACEBOOK" className="link" />
+          </a>
+          <a
+            href="https://x.com/m_ganesh_2946"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={twitter} alt="TWITTER" className="link" />
+          </a>
+          <a
+            href="https://www.instagram.com/ganesh_medichelimala/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={insta} alt="INSTAGRAM" className="link" />
+          </a>
+          <a
+            href="https://www.youtube.com/@ganesh_2946"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={youtube} alt="YOUTUBE" className="link" />
+          </a>
+          <a
+            href="https://github.com/ganeshmedichelimala"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={github} alt="GITHUB" className="link" />
+          </a>
+        </div>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,20 +1,52 @@
-import React from 'react'
-import './intro.css'
-import bgimage from '../../assets/imageout.png';
-import btnimage from '../../assets/hireme.png';
-import {Link} from 'react-scroll';
+import React from "react";
+import "./intro.css";
+import bgimage from "../../assets/imageout.png";
+import btnimage from "../../assets/hireme.png";
+import cvimage from "../../assets/cv.png";
+import { Link } from "react-scroll";
 
 const Intro = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/Ganesh_Medichelimala_CV.pdf"; // The path is relative to the public folder
+    link.download = "Ganesh_Medichelimala_CV.pdf"; // Specify the file name for download
+    link.click(); // Trigger the download
+  };
   return (
     <section id="intro">
-        <div className='introContent'>
-            <span className='hello'>Hello,  </span>
-            <span className='introtext'>I'm <span className='introname'>Ganesh Medichelimala</span><br/>Website Developer</span>
-            <p className='introPara'>I'm a skilled web developer with experince in creating <br/> fully functional full stack websites </p>        
-            <Link><button className='hirebtn'><img src={btnimage}alt="nio"  className='btnimg'/>Hire Me</button></Link>
-        </div>
+      <div className="introContent">
+        <span className="hello">Hello, </span>
+        <span className="introtext">
+          I'm <span className="introname">Ganesh Medichelimala</span>
+          <br />
+          Website Developer
+        </span>
+        <p className="introPara">
+          I'm a skilled web developer with experince in creating <br /> fully
+          functional full stack websites{" "}
+        </p>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          <button className="hirebtn">
+            <img src={btnimage} alt="nio" className="btnimg" />
+            Hire Me
+          </button>
+        </Link>
+        <Link>
+          <button className="cvbtn" onClick={handleDownload}>
+            <img src={cvimage} alt="Download CV" className="btnimg" />
+            Download CV
+          </button>
+        </Link>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Intro;
